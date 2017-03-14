24583d9f675ebd598979e7f33b733fefff9a40ed
Daniel Carcich <danielc@bccsoftware.com>
2017-03-14 13:30:59 -0400

    Remove video file feature (default to webcam)

--- main.py
+++ main.py
@@ -7,18 +7,12 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    camera = cv2.VideoCapture(0)
-    time.sleep(0.25)
-
-# otherwise, we are reading from a video file
-else:
-    camera = cv2.VideoCapture(args["video"])
+# default to webcam
+camera = cv2.VideoCapture(0)
+time.sleep(0.25)
 
 # initialize the first frame in the video stream
 firstFrame = None
